38d4515c1637fb6232daf8da04a5c3a3a75069c6
Samantha Barros <samantha.barros95@gmail.com>
2020-07-10 00:48:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ attr_accessor :name, :artist, :genre
 
   def initialize(name, artist = nil, genre = nil)
     @name = name
-    self.artist = artist
+    self.artist = artist if artist != nil
     self.genre = genre
     save
   end 
