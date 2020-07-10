481faaef42ebca682a62830f6cf8455cfda7d14b
Samantha Barros <samantha.barros95@gmail.com>
2020-07-10 00:56:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,28 +25,22 @@ attr_accessor :name, :artist, :genre
     self.new(name)
   end
   
-  def artist=(artist)
-    if @artist == nil
-      @artist = artist
-    else 
-      @artist = @artist
-    end 
-    if self.artist != nil
-      @artist.add_song(self)
-    end
+  def artist
     @artist
   end
-  
- def genre=(genre)
-    if @genre == nil
-      @genre = genre
-    else 
-      @genre = @genre
-    end 
-    if self.genre != nil
-      @genre.add_song(self)
-    end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre
     @genre
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
   end
   
   def self.create_from_filename(name)
